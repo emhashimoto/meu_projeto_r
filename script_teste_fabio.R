62b6fde86812d56552dd8cfed41a302e636d1102
Elizabeth Mie Hashimoto <emiehashimoto@gmail.com>
2026-08-24 11:33:59 -0300

    adiciona ggplot

--- script_teste_fabio.R
+++ script_teste_fabio.R
@@ -1,5 +1,16 @@
+##pacotes
+library(ggplot2)
+
 ##teste
 
-x <- rnorm(1000,3,1)
+x <- rnorm(1000, 3, 1)
 
 plot(x)
+
+##como criar um histograma no ggplot2
+dados <- data.frame(x)
+
+dados |>
+  ggplot() +
+  aes(x = x) +
+  geom_histogram()
